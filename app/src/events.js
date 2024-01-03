46c82d22d05a600b7b26118fc8bf367e0cdba0f6
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-01-03 20:18:51 +0530

    🔔 | added event listener

---
+++ app/src/events.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import chalk from "chalk";
+import instance from "./ethers.js";
+
+(() => {
+    console.log("Listening...");
+    instance.on("Issued", (course, id, date, event) => {
+        console.log(chalk.bgGreen('**** EVENT OCCURED ****'));
+        console.log("course:", course);
+        console.log("id:", id);
+        console.log("date:", date);
+        console.log("event:", event);
+        console.log(chalk.bgGreen('***********************'));
+    });
+})();
