b52f4f83c92af579223ed44081bf42260cdce07e
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-02-28 21:10:21 +0530

    🩹 | fixed parameter in event listener

--- src/events.js
+++ src/events.js
@@ -3,12 +3,12 @@ import chalk from "chalk";
 import instance from "./ethers.js";
 
 (() => {
-  console.log("Listening...");
-  instance.on("Issued", (course, id, date, event) => {
+  console.log(chalk.magenta("Listening for Issue Events..."));
+  instance.on("Issued", (course, id, grade, event) => {
     console.log(chalk.bgGreen("**** EVENT OCCURED ****"));
     console.log("course:", course);
     console.log("id:", id);
-    console.log("date:", date);
+    console.log("grade:", grade);
     console.log("event:", event);
     console.log(chalk.bgGreen("***********************"));
   });
